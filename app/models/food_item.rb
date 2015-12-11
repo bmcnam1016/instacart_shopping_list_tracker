501f4a7c09c99dfed03d601ca05b64405e2c59b9
bmcnam1016 <bmcnamar@chicagobooth.edu>
2015-12-11 11:43:31 -0600

    Added more validations, changed layout of some things.

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -2,4 +2,9 @@ class FoodItem < ActiveRecord::Base
   belongs_to :food_category
   belongs_to :user
   validates :user, :presence => true
+  validates :image, :presence => true
+  validates :current_amount, :presence => true, numericality: { only_integer: true }
+  validates :name, :presence => true
+  validates :reorder_amount, :presence => true, numericality: { only_integer: true }
+  validates :amount_per_order, :presence => true, numericality: { only_integer: true }
 end
